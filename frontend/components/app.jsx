67eccf7238cbe21d738ec69571f93164556cb7c8
Michael <mikemach123@gmail.com>
2017-03-17 15:11:55 -0700

    fix signin page styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import HeaderContainer from './header/header_container'
 
 const App = ({ children }) => (
   <div className='app'>
+    <HeaderContainer />
     { children }
   </div>
 );
--- frontend/components/board/board.jsx
+++ frontend/components/board/board.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import { Link, hashHistory } from 'react-router';
-import HeaderContainer from '../header/header_container'
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      boards: []
+    }
+
+    this.getBoardList = this.getBoardList.bind(this);
   }
 
   componentWillMount() {
     this.props.getCurrentUser().then((res) => {
-      this.props.getBoards()
+      this.props.getBoards().then((res) => {
+        let boards_list = this.getBoardList();
+        this.setState({boards: boards_list});
+      })
     });
   }
 
+  getBoardList() {
+    let boards = [];
+    Object.keys(this.props.boards).forEach((key) => {
+      let board = this.props.boards[key];
+      boards.push(
+        <li className="board_tile_item" key={board.id}>{board.name}</li>
+      )
+    })
+    return boards
+  }
+
   render() {
-    
 
     return(
-      <div>
-        <HeaderContainer />
-        <h1>Welcome to Boards</h1>
+      <div className="board_page">
+        <h1 className="board_title">Welcome to Boards</h1>
+        <ul className="board_tile_list">
+          {this.state.boards}
+        </ul>
       </div>
     )
   }
