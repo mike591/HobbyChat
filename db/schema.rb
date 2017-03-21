56de0891e2441c4a94caa7f5dd29033aa01d8010
Michael <mikemach123@gmail.com>
2017-03-21 12:57:02 -0700

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317161720) do
+ActiveRecord::Schema.define(version: 20170321195525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170317161720) do
   create_table "boards", force: :cascade do |t|
     t.string   "name"
     t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "post"
+    t.integer  "board_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
