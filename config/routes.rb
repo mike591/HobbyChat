ed45a88a0ac08f0c03ccc14d0e33e74adbb705b8
Michael <mikemach123@gmail.com>
2017-03-27 14:50:30 -0700

    unable to go back to homepage if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'home#show'
   get 'room/show'
   post 'room/create'
 
   get 'boards/index'
   get '/show_current_user', to: 'sessions#show_current_user'
-
-  root to: 'home#show'
-
   get 'dashboard', to: 'static_pages#show'
 
   get 'auth/google_oauth2', as: 'google_auth'
