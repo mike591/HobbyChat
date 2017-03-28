b92777132b8e13a5c5df83f9ca890bad2e067eec
Michael <mikemach123@gmail.com>
2017-03-28 13:04:44 -0700

    revert

--- frontend/hobbychat.jsx
+++ frontend/hobbychat.jsx
@@ -3,27 +3,12 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store.js';
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   console.log("dom loaded");
-//   const root = document.getElementById('root')
-//   let store = configureStore()
-//
-//   // TODO testing
-//   window.store = store;
-//
-//   ReactDOM.render(<Root store={store} />, root);
-// });
-
-function run() {
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root')
   let store = configureStore()
+
+  // TODO testing
   window.store = store;
-  ReactDOM.render(<Root store={store} />, document.getElementById('root'));
-}
 
-const loadedStates = ['complete', 'loaded', 'interactive'];
-
-if (loadedStates.includes(document.readyState) && document.body) {
-  run();
-} else {
-  window.addEventListener('DOMContentLoaded', run, false);
-}
+  ReactDOM.render(<Root store={store} />, root);
+});
