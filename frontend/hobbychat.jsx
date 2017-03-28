1c33bcd96af1b10e86f09119243c1f1f87398eb2
Michael <mikemach123@gmail.com>
2017-03-28 15:15:19 -0700

    add path const

--- frontend/hobbychat.jsx
+++ frontend/hobbychat.jsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store.js';
 
+console.log("hobbychat jsx loaded");
 
-let func = function (){
-    document.addEventListener("DOMContentLoaded", () => {
-    console.log("hello");
-    const root = document.getElementById('root')
-    let store = configureStore()
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root')
+  let store = configureStore()
 
-    // TODO testing
-    window.store = store;
+  // TODO testing
+  window.store = store;
 
-    ReactDOM.render(<Root store={store} />, root);
-  });
-}
-
-setInterval(func, 500)
+  ReactDOM.render(<Root store={store} />, root);
+});
