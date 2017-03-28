79c9d17c28aba5647f1d09cd827afd2b9ee1bc75
Michael <mikemach123@gmail.com>
2017-03-28 14:33:07 -0700

    testing single page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def show
-    if logged_in?
-      redirect_to dashboard_url
-    else
-      render :show
-    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to dashboard_url
+    # redirect_to dashboard_url
+    redirect_to root_url
   end
 
   def destroy
