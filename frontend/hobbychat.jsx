c4f98011a5b6dd085d4c75cb383f5ed64ce58337
Michael <mikemach123@gmail.com>
2017-03-28 13:02:52 -0700

    ensure domcontentloaded

--- frontend/hobbychat.jsx
+++ frontend/hobbychat.jsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("dom loaded");
-  const root = document.getElementById('root')
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("dom loaded");
+//   const root = document.getElementById('root')
+//   let store = configureStore()
+//
+//   // TODO testing
+//   window.store = store;
+//
+//   ReactDOM.render(<Root store={store} />, root);
+// });
+
+function run() {
   let store = configureStore()
-
-  // TODO testing
   window.store = store;
+  ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+}
 
-  ReactDOM.render(<Root store={store} />, root);
-});
+const loadedStates = ['complete', 'loaded', 'interactive'];
+
+if (loadedStates.includes(document.readyState) && document.body) {
+  run();
+} else {
+  window.addEventListener('DOMContentLoaded', run, false);
+}
