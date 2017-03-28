8d5ef87c5f2a3fa281dab1e6ac10a2612ebcd5f0
Michael <mikemach123@gmail.com>
2017-03-28 12:27:13 -0700

    show image son heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.serve_static_assets = true
   config.assets.compile = true
+  config.serve_static_files = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -25,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
