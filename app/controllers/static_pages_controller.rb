dde4ce769cb7f9c730eb53a4315e725e9cd18c24
Michael <mikemach123@gmail.com>
2017-03-29 14:18:53 -0700

    remove old files

--- app/controllers/static_pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class StaticPagesController < ApplicationController
-  before_filter :ensure_logged_in
-
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   get 'boards/index'
   get '/show_current_user', to: 'sessions#show_current_user'
-  get 'dashboard', to: 'static_pages#show'
 
   get 'auth/google_oauth2', as: 'google_auth'
   get 'auth/:provider/callback', to: 'sessions#create'
